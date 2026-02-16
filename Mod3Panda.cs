5f0ce914b1990f26db00c5e4c5a451d53fa978af
abrahamzerpa12-sys <abrahamzerpa12@gmail.com>
2026-02-15 22:08:45 -0500

    Add files via upload

---
+++ Mod3Panda.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Module3Demo
+{
+    public class Panda
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Panda(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"{Name} is eating {food}.");
+        }
+
+        public class PandaStats
+        {
+            public int BambooPerDay { get; }
+            public double HoursOfSleep { get; }
+
+            public PandaStats(int bambooPerDay, double hoursOfSleep)
+            {
+                BambooPerDay = bambooPerDay;
+                HoursOfSleep = hoursOfSleep;
+            }
+
+            public void PrintStats(Panda panda)
+            {
+                Console.WriteLine(
+                    $"{panda.Name} eats about {BambooPerDay} bamboo shoots per day " +
+                    $"and sleeps around {HoursOfSleep} hours.");
+            }
+        }
+    }
+
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Panda po = new Panda("Po", 5);
+
+            
+            po.Eat("bamboo");
+
+            
+            Panda.PandaStats stats = new Panda.PandaStats(bambooPerDay: 30, hoursOfSleep: 12.5);
+
+            
+            stats.PrintStats(po);
+        }
+    }
+}
